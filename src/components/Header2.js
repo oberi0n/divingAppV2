3d25ffaba237c2235b1e8863d3e02b480c174340
unknown <ncls.noel@gmail.com>
2023-10-30 17:31:26 +0100

    Fix links in header

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -75,7 +75,7 @@ function Header2() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex' },
